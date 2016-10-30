1fd65d4f0034f8b07267aaed29c3f7461ca9d767
wangzaieee <1426366264@qq.com>
2016-10-30 17:59:33 +0800

    。。。。

---
+++ coordinator_toolbar_tab/src/main/java/com/lwapp/luowang/coordinator_toolbar_tab/news/fragment/MilitaryFragment.java
@@ -0,0 +1,15 @@
+package com.lwapp.luowang.coordinator_toolbar_tab.news.fragment;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MilitaryFragment extends BaseFragment {
+
+    @Override
+    public String getUrl() {
+        return "军事";
+    }
+}
